9e4b7f393aee281a00ddcd07be252181189fee47
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-01-25 18:29:47 +0530

    separate layout

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const port = 3003
 
 const server = app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+app.set("view engine", "pug")
+app.set("views", "views")
+
 app.get("/", (req, res, next) => {
-    res.status(200).send("Guguuuuuuu")
+    const payload = {
+        pageTitle: "Home"
+    }
+    res.status(200).render("home", payload)
 })
 
