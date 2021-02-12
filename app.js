4433daa2fc86baf7ffc002fd8bde5fcd8b708935
Pardip Bhatti <pardipbhatti8791@gmail.com>
2021-02-12 18:33:20 +0530

    changes in dashboard

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const path = require('path')
+const express = require('express');
+const app = express();
+const port = 3003;
 const middleware = require('./middleware')
-const mongoose = require("./database")
-const app = express()
-const port = 3003
+const path = require('path')
+const bodyParser = require("body-parser")
+const mongoose = require("./database");
+const session = require("express-session");
 
+const server = app.listen(port, () => console.log("Server listening on port " + port));
 
+app.set("view engine", "pug");
+app.set("views", "views");
 
-const server = app.listen(port, () => console.log(`Server listening on port ${port}`))
-
-app.set("view engine", "pug")
-app.set("views", "views")
-app.use(express.static(path.join(__dirname, "public"), ))
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
     secret: "bbq chips",
@@ -25,17 +22,18 @@ app.use(session({
 }))
 
 // Routes
-const loginRoute = require('./routes/loginRoute')
-const registerRoute = require('./routes/registerRoute')
+const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
 
-app.use("/login", loginRoute)
-app.use("/register", registerRoute)
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
-    const payload = {
-        pageTitle: "Home",
-        userLoggedIn:  req.session.user
-    }
-    return res.status(200).render("home", payload)
-})
 
+    var payload = {
+        pageTitle: "Home",
+        userLoggedIn: req.session.user
+    }
+
+    res.status(200).render("home", payload);
+})
